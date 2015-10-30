4d88adbd73000ac3334e2b37557cd9b28645d90a
Sean Marshallsay <srm.1708@gmail.com>
2015-10-30 11:26:39 +0000

    Comments and formatting.

--- HPC/Q3/C/floats.c
+++ HPC/Q3/C/floats.c
@@ -13,10 +13,9 @@ static const char* TEN_BILLION_NINTHS = "1111111111.1111111111111111111111111111
 
 void float_precision(void);
 void double_precision(void);
-int number_correct_digits(const char* result, const char* reference);
+int number_correct_digits_float(const char* result, const char* reference);
 
 int main(int argc, char* argv[]) {
-    // Note I'm using digits not decimal places, I assumed this was what was wanted.
     float_precision();
     double_precision();
 
@@ -29,19 +28,25 @@ void float_precision() {
     char* str_at_zero = calloc(LENGTH, sizeof(char));
     // Convert float to string with 100 decimal places.
     snprintf(str_at_zero, LENGTH, "%0.100f", at_zero);
-    printf("Near zero, floats have about %d digits of precision.\n", number_correct_digits(str_at_zero, ONE_NINTH));
+    printf(
+        "Near zero, floats have about %d digits of precision.\n",
+        number_correct_digits(str_at_zero, ONE_NINTH),
+    );
+    free(str_at_zero);
 
     // Ten million divided by nine.
     float at_million = (float)10000000/(float)9;
     char* str_at_million = calloc(LENGTH, sizeof(char));
     snprintf(str_at_million, LENGTH, "%0.100f", at_million);
     printf("Near one million, floats have about %d digits of precision.\n", number_correct_digits(str_at_million, TEN_MILLION_NINTHS));
+    free(str_at_million);
 
     // Ten billion divided by nine.
     float at_billion = (float)10000000000/(float)9;
     char* str_at_billion = calloc(LENGTH, sizeof(char));
     snprintf(str_at_billion, LENGTH, "%0.100f", at_billion);
     printf("Near one billion, floats have about %d digits of precision.\n", number_correct_digits(str_at_billion, TEN_BILLION_NINTHS));
+    free(str_at_billion);
 }
 
 void double_precision() {
@@ -49,21 +54,26 @@ void double_precision() {
     char* str_at_zero = calloc(LENGTH, sizeof(char));
     snprintf(str_at_zero, LENGTH, "%0.100lf", at_zero);
     printf("Near zero, doubles have about %d digits of precision.\n", number_correct_digits(str_at_zero, ONE_NINTH));
+    free(str_at_zero);
 
     // Ten million divided by nine.
     double at_million = (double)10000000/(double)9;
     char* str_at_million = calloc(LENGTH, sizeof(char));
     snprintf(str_at_million, LENGTH, "%0.100lf", at_million);
     printf("Near one million, doubles have about %d digits of precision.\n", number_correct_digits(str_at_million, TEN_MILLION_NINTHS));
+    free(str_at_million);
 
     // Ten billion divided by nine.
     double at_billion = (double)10000000000/(double)9;
     char* str_at_billion = calloc(LENGTH, sizeof(char));
     snprintf(str_at_billion, LENGTH, "%0.100lf", at_billion);
     printf("Near one billion, doubles have about %d digits of precision.\n", number_correct_digits(str_at_billion, TEN_BILLION_NINTHS));
+    free(str_at_billion);
 }
 
-int number_correct_digits(const char* result, const char* reference) {
+// Note that this functions counts the number of correct digits in a float, NOT
+// the number of correct decimal places.
+int number_correct_digits_float(const char* result, const char* reference) {
     // Avoid out-of-bounds access.
     int res_len = strlen(result);
     int ref_len = strlen(reference);
