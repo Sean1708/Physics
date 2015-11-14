a1e5a1f4cced4d67feac5a42eaf59c8d447a7705
Sean Marshallsay <srm.1708@gmail.com>
2015-11-14 16:47:25 +0000

    Working serial implementation.

--- HPC/Q6/C/openmp.c
+++ HPC/Q6/C/openmp.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+void time_kahan(size_t N);
+double kahan(size_t N, double* A, double* B);
 
 int main(int argc, char* argv[]) {
-    printf("Hello %s!\n", argv[0]);
+    size_t Ns[] = {1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000, 2000000, 5000000};
+    size_t num_Ns = (size_t)(sizeof(Ns)/sizeof(Ns[0]));
+    for (size_t i = 0; i < num_Ns; i++) {
+        size_t N = Ns[i];
+        time_kahan(N);
+    }
 
     return 0;
+}
+
+void time_kahan(size_t N) {
+    double* A = calloc(N, sizeof(double));
+    double* B = calloc(N, sizeof(double));
+
+    clock_t start = clock();
+    double sum = kahan(N, A, B);
+    clock_t stop = clock();
+    double diff = (double)(stop - start)/CLOCKS_PER_SEC;
+
+    printf("       N: %lu\n", (unsigned long)N);
+    printf("     sum: %lf\n", sum);
+    printf("time (s): %lf\n", diff);
+    printf("\n");
+
+    free(A);
+    free(B);
+}
+
+double kahan(size_t N, double* A, double* B) {
+    double sum = 0;
+    for (size_t i = 0; i < N; i++) {
+        A[i] = (double)i;
+        B[i] = A[i]*A[i];
+        A[i] += sqrt(fabs(sin(B[i])))*2.34;
+        sum += A[i];
+    }
+
+    return sum;
 }
